90d4b252afbcaac7d83ff849a077074b9ca918e1
Rishi Raj Jaiswal <rishirajjaiswal0103.com>
2026-02-22 18:30:17 +0530

    refactor(app): migrate from Context API to Redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import AppRoutes from "./routes/AppRoutes";
-import { AuthProvider } from "./context/AuthContext";
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
-  return (
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
